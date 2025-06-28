c95915640b8bfa20e816184ded5ac942470b03f6
beimnet melese <beimnetmelese16@gmail.com>
2025-06-28 18:17:01 +0300

    bug fixs

--- src/app/api/jobs/[id]/route.ts
+++ src/app/api/jobs/[id]/route.ts
@@ -1,47 +1,51 @@
 import { NextResponse } from "next/server";
-import { JobApplication } from "@/app/lib/constants";
 
-// Mock data
-const mockJobs: JobApplication[] = [
+const mockJobs = [
   {
-    id: '1',
-    company: 'TechCorp',
-    position: 'Frontend Developer',
-    url: 'https://example.com/job/1',
-    salary: '$90,000 - $120,000',
-    status: 'interview',
-    description: 'Looking for an experienced React developer...',
-    dateApplied: '2023-05-15',
-    notes: 'Second interview scheduled for next week',
-    contact: 'recruiter@techcorp.com',
+    id: "1",
+    company: "TechCorp",
+    position: "Frontend Developer",
+    url: "https://example.com/job/1",
+    salary: "$90,000 - $120,000",
+    status: "interview",
+    description: "Looking for an experienced React developer...",
+    dateApplied: "2023-05-15",
+    notes: "Second interview scheduled for next week",
+    contact: "recruiter@techcorp.com",
   },
   {
-    id: '2',
-    company: 'TechCorp',
-    position: 'Frontend Developer',
-    url: 'https://example.com/job/2',
-    salary: '$90,000 - $120,000',
-    status: 'applied',
-    description: 'Looking for an experienced React developer...',
-    dateApplied: '2023-05-15',
-    notes: 'Second interview scheduled for next week',
-    contact: 'recruiter@techcorp.com',
+    id: "2",
+    company: "TechCorp",
+    position: "Frontend Developer",
+    url: "https://example.com/job/2",
+    salary: "$90,000 - $120,000",
+    status: "applied",
+    description: "Looking for an experienced React developer...",
+    dateApplied: "2023-05-15",
+    notes: "Second interview scheduled for next week",
+    contact: "recruiter@techcorp.com",
   },
   {
-    id: '3',
-    company: 'TechCorp',
-    position: 'Frontend Developer',
-    url: 'https://example.com/job/3',
-    salary: '$90,000 - $120,000',
-    status: 'offer',
-    description: 'Looking for an experienced React developer...',
-    dateApplied: '2023-05-15',
-    notes: 'Second interview scheduled for next week',
-    contact: 'recruiter@techcorp.com',
+    id: "3",
+    company: "TechCorp",
+    position: "Frontend Developer",
+    url: "https://example.com/job/3",
+    salary: "$90,000 - $120,000",
+    status: "offer",
+    description: "Looking for an experienced React developer...",
+    dateApplied: "2023-05-15",
+    notes: "Second interview scheduled for next week",
+    contact: "recruiter@techcorp.com",
   },
 ];
 
-export async function GET(req: Request, context: { params: { [key: string]: string } }) {
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function GET(req: Request, context: Context) {
   const { id } = context.params;
 
   const job = mockJobs.find((job) => job.id === id);
